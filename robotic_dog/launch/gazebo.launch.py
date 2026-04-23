f19a30064c4ae2a130c3e2690be4d038c4b85f8d
PATO <felipiniobrsp@gmail.com>
2026-04-23 14:53:24 -0300

    Update gazebo.launch.py

--- robotic_dog/launch/gazebo.launch.py
+++ robotic_dog/launch/gazebo.launch.py
@@ -1,7 +1,7 @@
 from launch import LaunchDescription
 from launch.actions import IncludeLaunchDescription, TimerAction
 from launch.launch_description_sources import PythonLaunchDescriptionSource
-from launch_ros.actions import Node
+from launch_ros.actions import Node, SetParameter
 from ament_index_python.packages import get_package_share_directory
 import os
 
@@ -10,7 +10,9 @@ def generate_launch_description():
     urdf_path = os.path.join(pkg_dir, 'urdf', 'robot.urdf')
 
     return LaunchDescription([
-        # Gazebo
+
+        SetParameter(name='use_sim_time', value=True),
+        # 1. Gazebo
         IncludeLaunchDescription(
             PythonLaunchDescriptionSource(
                 os.path.join(get_package_share_directory('ros_gz_sim'), 'launch', 'gz_sim.launch.py')
@@ -18,57 +20,85 @@ def generate_launch_description():
             launch_arguments={'gz_args': '-r empty.sdf'}.items()
         ),
 
-        # Publica URDF
+        # 2. Bridge (clock + tf + odom)
         Node(
-            package='robot_state_publisher',
-            executable='robot_state_publisher',
-            parameters=[{'robot_description': open(urdf_path, 'r').read()},{'use_sim_time': True}],
-            output='screen',
-        ),
-
-        # Spawnea o robô
-
-        Node(
-            package='ros_gz_sim',
-            executable='create',
+            package='ros_gz_bridge',
+            executable='parameter_bridge',
             arguments=[
-                '-topic', '/robot_description',
-                '-name', 'robotic_dog',
-                '-allow_renaming', 'false',
-                '-x', '0.0',
-                '-y', '0.0',
-                '-z', '1.5',
-                '-Y', '0.0'
+                '/clock@rosgraph_msgs/msg/Clock[gz.msgs.Clock',
+                '/odom@nav_msgs/msg/Odometry[gz.msgs.Odometry',
+                '/imu@sensor_msgs/msg/Imu[gz.msgs.IMU',
             ],
-            output='screen'
-        ),
-
-        # Controller Manager (carrega ros2_control)
-        Node(
-            package='controller_manager',
-            executable='ros2_control_node',
-            parameters=[os.path.join(pkg_dir, 'config', 'controllers.yaml'), {'use_sim_time': True}],
+            parameters=[{'use_sim_time': True}],
             output='screen',
         ),
 
-        # Spawna os controladores (com delay para esperar hardware)
         TimerAction(
-            period=8.0,  # 8 segundos de delay
+            period=4.0,   
             actions=[
                 Node(
-                    package='controller_manager',
-                    executable='spawner',
-                    arguments=['joint_state_broadcaster', '--controller-manager', '/controller_manager'],
+                    package='robot_state_publisher',
+                    executable='robot_state_publisher',
+                    parameters=[
+                        {'robot_description': open(urdf_path, 'r').read()},
+                        {'use_sim_time': True}
+                    ],
                     output='screen',
-                    parameters=[{'use_sim_time': True}]
                 ),
+
+                # Spawn the robot
+                Node(
+                    package='ros_gz_sim',
+                    executable='create',
+                    arguments=[
+                        '-topic', '/robot_description',
+                        '-name', 'robotic_dog',
+                        '-allow_renaming', 'false',
+                        '-x', '0.0',
+                        '-y', '0.0',
+                        '-z', '1.5',
+                        '-Y', '0.0'
+                    ],
+                    parameters=[{'use_sim_time': True}],
+                    output='screen'
+                ),
+
+                # Controller Manager
                 Node(
                     package='controller_manager',
-                    executable='spawner',
-                    arguments=['position_controllers', '--controller-manager', '/controller_manager'],
+                    executable='ros2_control_node',
+                    parameters=[
+                        os.path.join(pkg_dir, 'config', 'controllers.yaml'),
+                        {'use_sim_time': True}
+                    ],
                     output='screen',
-                    parameters=[{'use_sim_time': True}]
                 ),
+                TimerAction(
+                period=6.0,
+                actions=[
+                    Node(
+                        package='controller_manager',
+                        executable='spawner',
+                        arguments=['joint_state_broadcaster', '--controller-manager', '/controller_manager'],
+                        output='screen',
+                        parameters=[{'use_sim_time': True}]
+                    ),
+                    Node(
+                        package='controller_manager',
+                        executable='spawner',
+                        arguments=['position_controllers', '--controller-manager', '/controller_manager'],
+                        output='screen',
+                        parameters=[{'use_sim_time': True}]
+                    ),
+                    Node(
+                        package='robotic_dog',
+                        executable='robotic_dog_node',
+                        name='robotic_dog_node',
+                        parameters=[{'use_sim_time': True}],
+                        output='screen',
+                    ),
+                ]
+            ),
             ]
         ),
     ])
